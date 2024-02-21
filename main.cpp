3f5ddceac80ca086d585151858603ef8293d11ec
Nikita <hailrake04@gmail.com>
2024-02-21 15:41:20 -0500

    ex. 17.3

--- main.cpp
+++ main.cpp
@@ -1,15 +1,21 @@
+#include <array>
 #include <iostream>
-#include <fstream>
 
-int main() {
-
-    std::ofstream testFile("test.txt");
-
-    for (int b = 1; b <= 10; ++b) {
-        testFile << b << '\n';
+template<typename T, std::size_t U>
+void printArray(const std::array<T, U>& inputArray) {
+    for (T val : inputArray) {
+        std::cout << val;
     }
+    std::cout << "\narray done\n";
+}
 
-    testFile.close();
+int main()
+{
+    constexpr std::array arr1 { 1, 4, 9, 16 };
+    printArray(arr1);
+
+    constexpr std::array arr2 { 'h', 'e', 'l', 'l', 'o' };
+    printArray(arr2);
 
     return 0;
 }
