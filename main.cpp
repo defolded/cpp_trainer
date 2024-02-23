e128932b5c5e10109763be71904ff98c176ec098
Nikita <hailrake04@gmail.com>
2024-02-22 21:09:17 -0500

    ex. 17.10

--- main.cpp
+++ main.cpp
@@ -1,33 +1,15 @@
-#include <array>
 #include <iostream>
+
+void printString(const char* s) {
+    while (*s != '\0') {
+        std::cout << *s;
+        ++s;
+    }
+}
 
 int main()
 {
-    constexpr int numbers[] { 0, 1, 4, 9 };
-
-    while (true) {
-        std::cout << "Enter a single digit integer, or -1 to quit: ";
-        int x{};
-        std::cin >> x;
-
-        if (x == -1) {
-            std::cout << "Bye\n";
-            break;
-        } else if (x <= numbers[std::ssize(numbers)-1]) {
-            for (auto i : numbers) {
-                if (x == i) {
-                    std::cout << x << " is a perfect square\n";
-                    break;
-                } else {
-                    std::cout << x << " is not a perfect square\n";
-                    break;
-                }
-            }
-        } else {
-            std::cerr << "Error\n";
-            return 1;
-        }
-    }
+    printString("Hello World!");
 
     return 0;
 }
