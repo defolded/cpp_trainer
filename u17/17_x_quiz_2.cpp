72eef0b6aa28afc7ce7ae1282999b334c5284a48
Nikita <hailrake04@gmail.com>
2024-02-24 12:09:25 -0500

    ex. 17.x (quiz 2)

--- u17/17_x_quiz_2.cpp
+++ u17/17_x_quiz_2.cpp
@@ -5,8 +5,10 @@
 
 //  Question #2
 
-namespace Potion {
-    enum Type {
+namespace Potion
+{
+    enum Type
+    {
         healing,
         mana,
         speed,
@@ -25,7 +27,8 @@ namespace Potion {
     static_assert(std::size(entrees) == max_potions);
 }
 
-class Player {
+class Player
+{
 public:
     explicit Player(std::string_view name) :
         m_name { name },
@@ -35,7 +38,21 @@ public:
 
     [[nodiscard]] int gold() const { return m_gold; }
     [[nodiscard]] std::string_view name() const { return m_name; }
-    [[nodiscard]] int inventory(Potion::Type t) { return m_inventory[t]; }
+    [[nodiscard]] int inventory(int t) { return m_inventory[t]; }
+
+    bool buy(const Potion::Type item)
+    {
+        if (m_gold >= Potion::prices[item])
+        {
+            m_gold -= Potion::prices[item];
+            ++m_inventory[item];
+            return true;
+        } else
+        {
+            std::cout << "You can not afford that.\n";
+            return false;
+        }
+    }
 
 private:
     std::string_view m_name;
@@ -43,27 +60,85 @@ private:
     std::array<int, Potion::max_potions> m_inventory {};
 };
 
-void process(Player& client) {
-    while (true) {
-        std::cout << "Enter the number of the potion you'd like to buy, or 'q' to quit:";
-        char c {};
-        std::cin >> c;
+void ignoreLine()
+{
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
 
+int charNumToInt(char c)
+{
+    return c - '0';
+}
+
+void process(Player& client)
+{
+    char input{};
+    while (true)
+    {
+        std::cout << "Enter the number of the potion you'd like to buy, or 'q' to quit:";
+        std::cin >> input;
+        if (!std::cin)
+        {
+            std::cin.clear(); // put us back in 'normal' operation mode
+            ignoreLine(); // and remove the bad input
+            continue;
+        }
+
+        // If there is extraneous input, treat as failure case
+        if (!std::cin.eof() && std::cin.peek() != '\n')
+        {
+            std::cout << "I didn't understand what you said.  Try again.\n";
+            ignoreLine(); // ignore any extraneous input
+            continue;
+        }
+
+        if (input == 'q')
+            break;
+
+        // Convert the char to a number and see if it's a valid potion selection
+        int val { charNumToInt(input) };
+        if (val >= 0 && val < Potion::max_potions)
+        {
+            client.buy(static_cast<Potion::Type>(val));
+            std::cout << "You purchased a potion of " << Potion::entrees[static_cast<Potion::Type>(val)] << ". You have " << client.gold() << " gold left.\n\n";
+        } else
+        {
+            // It wasn't a valid potion selection
+            std::cout << "I didn't understand what you said.  Try again.\n";
+            ignoreLine();
+        }
     }
 }
 
-void shop(Player& client) {
+void printInventory(Player& client)
+{
+    for (auto p: Potion::types)
+    {
+        if (client.inventory(p) > 0)
+            std::cout << client.inventory(p) << "x potion of " << Potion::entrees[p] << '\n';
+    }
+
+    std::cout << "You escaped with " << client.gold() << " gold remaining.\n";
+}
+
+void shop(Player& client)
+{
     std::cout << "\nHere is our selection for today:\n";
     for (auto p: Potion::types)
         std::cout << p << ") " << Potion::entrees[p] << " costs " << Potion::prices[p] << '\n';
 
     process(client);
+
+    printInventory(client);
+    std::cout << "\nThanks for shopping at Roscoe's potion emporium!";
 }
 
-int main() {
+int main()
+{
     std::cout << "Welcome to Roscoe's potion emporium!\n";
 
     std::cout << "Enter your name:";
+
     std::string name{};
     std::getline(std::cin >> std::ws, name); // read a full line of text into name
 
@@ -72,8 +147,6 @@ int main() {
     std::cout << "Hello, " << player.name() << " you have " << player.gold() << " gold.\n";
 
     shop(player);
-
-    std::cout << "\nThanks for shopping at Roscoe's potion emporium!";
 
     return 0;
 }
