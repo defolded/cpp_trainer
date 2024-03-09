3b3ea2705cc99b6b94fc8212b6254bbf12028f2c
Nikita <hailrake04@gmail.com>
2024-03-09 12:32:18 -0500

    ex. 21.x (quiz 4)

--- u21/21_x_quiz_4.cpp
+++ u21/21_x_quiz_4.cpp
@@ -1,9 +1,9 @@
 #include <cassert>
 #include <cstdint> // for fixed width integers
 #include <iostream>
+#include <cmath>
 
-class FixedPoint2
-{
+class FixedPoint2 {
 private:
     std::int16_t m_base{}; // here's our non-fractional part
     std::int8_t m_decimal{}; // here's our factional part
@@ -12,21 +12,43 @@ public:
     FixedPoint2(std::int16_t base = 0, std::int8_t decimal = 0)
             : m_base{ base }, m_decimal{ decimal }
     {
-        // If either (or both) of the non-fractional and fractional part of the number are negative, the number should be treated as negative
         if (m_base < 0 || m_decimal < 0)
         {
-            // Make sure base is negative
             if (m_base > 0)
                 m_base = -m_base;
-            // Make sure decimal is negative
             if (m_decimal > 0)
                 m_decimal = -m_decimal;
         }
+
+        m_base += m_decimal / 100;
+        m_decimal = m_decimal % 100;
+    }
+
+    FixedPoint2(double d) :
+            FixedPoint2(
+                    static_cast<std::int16_t>(std::trunc(d)),
+                    static_cast<std::int8_t>(std::round(d * 100) - std::trunc(d) * 100)
+            )
+    {
     }
 
     explicit operator double() const
     {
         return m_base + (static_cast<double>(m_decimal) / 100);
+    }
+
+    friend bool operator== (const FixedPoint2& fp1, const FixedPoint2& fp2)
+    {
+        return (fp1.m_base == fp2.m_base && fp1.m_decimal == fp2.m_decimal);
+    }
+
+    FixedPoint2 operator- () const
+    {
+        return FixedPoint2
+        {
+            static_cast<std::int16_t>(-m_base),
+            static_cast<std::int8_t>(-m_decimal)
+        };
     }
 };
 
@@ -37,27 +59,46 @@ std::ostream& operator<<(std::ostream& out, const FixedPoint2& fp)
     return out;
 }
 
+std::istream& operator>>(std::istream& in, FixedPoint2& fp)
+{
+    double d{};
+    in >> d;
+    fp = FixedPoint2{ d };
+
+    return in;
+}
+
+FixedPoint2 operator+(const FixedPoint2& fp1, const FixedPoint2& fp2)
+{
+    return FixedPoint2
+            {
+                static_cast<double>(fp1) + static_cast<double>(fp2)
+            };
+}
+
 int main()
 {
-    FixedPoint2 a{ 34, 56 };
-    std::cout << a << '\n';
-    std::cout << static_cast<double>(a) << '\n';
-    assert(static_cast<double>(a) == 34.56);
+    assert(FixedPoint2{ 0.75 } == FixedPoint2{ 0.75 });    // Test equality true
+    assert(!(FixedPoint2{ 0.75 } == FixedPoint2{ 0.76 })); // Test equality false
 
-    FixedPoint2 b{ -2, 8 };
-    assert(static_cast<double>(b) == -2.08);
+    // Test additional cases -- h/t to reader Sharjeel Safdar for these test cases
+    assert(FixedPoint2{ 0.75 } + FixedPoint2{ 1.23 } == FixedPoint2{ 1.98 });    // both positive, no decimal overflow
+    assert(FixedPoint2{ 0.75 } + FixedPoint2{ 1.50 } == FixedPoint2{ 2.25 });    // both positive, with decimal overflow
+    assert(FixedPoint2{ -0.75 } + FixedPoint2{ -1.23 } == FixedPoint2{ -1.98 }); // both negative, no decimal overflow
+    assert(FixedPoint2{ -0.75 } + FixedPoint2{ -1.50 } == FixedPoint2{ -2.25 }); // both negative, with decimal overflow
+    assert(FixedPoint2{ 0.75 } + FixedPoint2{ -1.23 } == FixedPoint2{ -0.48 });  // second negative, no decimal overflow
+    assert(FixedPoint2{ 0.75 } + FixedPoint2{ -1.50 } == FixedPoint2{ -0.75 });  // second negative, possible decimal overflow
+    assert(FixedPoint2{ -0.75 } + FixedPoint2{ 1.23 } == FixedPoint2{ 0.48 });   // first negative, no decimal overflow
+    assert(FixedPoint2{ -0.75 } + FixedPoint2{ 1.50 } == FixedPoint2{ 0.75 });   // first negative, possible decimal overflow
 
-    FixedPoint2 c{ 2, -8 };
-    assert(static_cast<double>(c) == -2.08);
+    FixedPoint2 a{ -0.48 };
+    assert(static_cast<double>(a) == -0.48);
+    assert(static_cast<double>(-a) == 0.48);
 
-    FixedPoint2 d{ -2, -8 };
-    assert(static_cast<double>(d) == -2.08);
-
-    FixedPoint2 e{ 0, -5 };
-    assert(static_cast<double>(e) == -0.05);
-
-    FixedPoint2 f{ 0, 10 };
-    assert(static_cast<double>(f) == 0.1);
+    std::cout << "Enter a number: "; // enter 5.678
+    std::cin >> a;
+    std::cout << "You entered: " << a << '\n';
+    assert(static_cast<double>(a) == 5.68);
 
     return 0;
 }
