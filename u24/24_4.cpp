932a1f5c050014218de636e0a77932a59fd8dec8
Nikita <hailrake04@gmail.com>
2024-03-15 21:28:25 -0400

    ex. 24.4

---
+++ u24/24_4.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <utility>
+
+class Fruit
+{
+private:
+    std::string m_name{};
+    std::string m_color{};
+public:
+    explicit Fruit(std::string  name = " ", std::string  color = " ")
+        : m_name{std::move( name )}, m_color{std::move( color )}
+    {}
+
+    const std::string getName() const { return m_name; }
+    const std::string getColor() const { return m_color; }
+};
+
+class Apple : public Fruit
+{
+private:
+    double m_fiber{};
+public:
+    explicit Apple(const std::string& name = " ", const std::string& color = " ", const double& fiber = 0)
+        : Fruit(name, color),
+        m_fiber{ fiber }
+    {
+    }
+
+    const double getFiber() const { return m_fiber; }
+};
+
+std::ostream& operator<<(std::ostream& out, const Apple& apple)
+{
+    out << "Apple(" << apple.getName() << ", " << apple.getColor() << ", " << apple.getFiber() << ")";
+
+    return out;
+}
+
+class Banana : public Fruit
+{
+public:
+    explicit Banana(const std::string& name = " ", const std::string& color = " ")
+        : Fruit(name, color)
+    {}
+};
+
+std::ostream& operator<<(std::ostream& out, const Banana& banana)
+{
+    out << "Banana(" << banana.getName() << ", " << banana.getColor() << ")";
+
+    return out;
+}
+
+int main()
+{
+    const Apple a{ "Red delicious", "red", 4.2 };
+    std::cout << a << '\n';
+
+    const Banana b{ "Cavendish", "yellow" };
+    std::cout << b << '\n';
+
+    return 0;
+}
