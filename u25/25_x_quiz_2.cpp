c1992ba63c13fef62261fdab76190dc3445ee1c0
Nikita <hailrake04@gmail.com>
2024-03-19 19:03:21 -0400

    ex. 25.x (quiz)

---
+++ u25/25_x_quiz_2.cpp
@@ -0,0 +1,92 @@
+#include <vector>
+#include <iostream>
+
+class Point
+{
+protected:
+    int m_x{};
+    int m_y{};
+
+public:
+    explicit Point(int x = 0, int y = 0)
+        : m_x{x}, m_y{y}
+    {}
+
+    friend std::ostream& operator<<(std::ostream& out, const Point& p)
+    {
+        return out << "Point(" << p.m_x << ", " << p.m_y << ')';
+    }
+};
+
+class Shape
+{
+public:
+    virtual std::ostream& print(std::ostream& out) const = 0;
+
+    friend std::ostream& operator<<(std::ostream& out, const Shape& sh)
+    {
+        return sh.print(out);
+    }
+
+    virtual ~Shape() = default;
+};
+
+class Triangle : public Shape
+{
+protected:
+    Point m_one{};
+    Point m_two{};
+    Point m_three{};
+
+public:
+    Triangle(const Point& p1, const Point& p2, const Point& p3)
+        : m_one{p1}, m_two{p2}, m_three{p3}
+    {}
+
+    std::ostream& print(std::ostream& out) const override
+    {
+        out << "Triangle(" << m_one << ", " << m_two << ", " << m_three << ")";
+
+        return out;
+    }
+};
+
+class Circle : public Shape
+{
+protected:
+    Point m_point{};
+    int m_radius{};
+
+public:
+    Circle(const Point &p, int r)
+            : m_point{p}, m_radius{r} {}
+
+    std::ostream& print(std::ostream& out) const override
+    {
+        out << "Circle(" << m_point << " , radius " << m_radius << ")";
+
+        return out;
+    }
+
+};
+
+int main()
+{
+    std::vector<Shape*> v{
+            new Circle{Point{ 1, 2 }, 7},
+            new Triangle{Point{ 1, 2 }, Point{ 3, 4 }, Point{ 5, 6 }},
+            new Circle{Point{ 7, 8 }, 3}
+    };
+
+    // print each shape in vector v on its own line here
+    for (const auto* sh : v)
+        std::cout << *sh << "\n";
+
+//    std::cout << "The largest radius is: " << getLargestRadius(v) << '\n'; // TODO: write this function
+
+    // delete each element in the vector here
+    for (const auto* sh : v)
+        delete sh;
+
+    return 0;
+}
